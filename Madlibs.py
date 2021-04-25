492b7b53afbae38744795dcfa73ff49b892d5f65
zorrokid2 <82954670+zorrokid2@users.noreply.github.com>
2021-04-25 15:37:42 +0200

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,12 @@
+# Created by Robin Demesmaeker
+# Hope you enjoy
+
+Noun = input("Enter a plural noun: ")
+Adverb = input("Enter an adverb: ")
+Verb = input("Enter a verb in the first person that describes a feeling: ")
+Adjec = input("Enter an adjective: ")
+
+print("I love {}.".format(Noun))
+print("They are {}.".format(Adverb))
+print("I just {} them.".format(Verb))
+print("Those {} {}.".format(Adjec, Noun))
